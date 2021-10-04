c6c3a60778328e1252e63d1c9b0d3f9ca7c8435a
Ngoc Phu <phunobi1807@gmail.com>
2021-10-04 20:40:07 +0700

    ngFor-ngModel-ngClass

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,20 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   public name = 'Phu';
   public age = 25;
+  public traiCay = ['Táo', 'Nho', 'Cam', 'Quýt'];
+  public traiCay2 = [
+    { ten: 'Táo', gia: 12, haGia: true },
+    { ten: 'Nho', gia: -3, haGia: false },
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log('trai cay = ', this.traiCay);
   }
-  public resetName():void {
-    console.log("asf")
-    this.name = 'Phu';
-  }
+  // public resetName():void {
+  //   console.log("asf")
+  //   this.name = 'Phu';
+  // }
 
 }
